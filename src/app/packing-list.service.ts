894ed2a337dca706e1121b1302e546f841667950
Rosanna <rosannawagner14@gmail.com>
2023-03-06 11:02:11 +0100

    Added Code in packing-list.service.ts

--- src/app/packing-list.service.ts
+++ src/app/packing-list.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import Dexie from 'dexie';
+import { PackingList } from './packing-list';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PackingListService {
+export class PackingListService extends Dexie {
+  listitem!: Dexie.Table<PackingList, string>;
+  constructor() { 
+    super("packing-list-db");
+    this.version(1).stores({
+      listitem: 'id'
+    })
+}
 
-  constructor() { }
+  async add(title: string){
+    const packinglist = {title, id: crypto.randomUUID(), done:false };
+    await this.listitem.add(packinglist);
+  }
+
+  async getAll(){
+    return await this.listitem.toArray();
+  }
+
+  async bulkPut(listitem: PackingList[]) {
+  await this.listitem.bulkPut(listitem);
+  }
 }
