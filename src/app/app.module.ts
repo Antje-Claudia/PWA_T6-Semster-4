ab002624fd9602ae4ac2f00c41596285d4861705
Rosanna <rosannawagner14@gmail.com>
2023-03-06 09:55:30 +0100

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PackingListComponent } from './packing-list/packing-list.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PackingListComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
