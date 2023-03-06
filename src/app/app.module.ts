e1b0be1f933aa7aeaf4772bfe806956c8bd8656a
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2023-03-06 10:19:19 +0100

    Added Nav Component and added two links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PackingListComponent } from './packing-list/packing-list.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     PackingListComponent,
-    ImprintComponent
+    ImprintComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +31,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
